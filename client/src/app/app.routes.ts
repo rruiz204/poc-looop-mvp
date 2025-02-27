44a9fd5e9038890967514572c23ad0c0506125d4
roney ruiz <roneyruizrojas@gmail.com>
2025-02-26 20:42:04 -0600

    feat(server): add routing for test page

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomePageComponent } from './modules/home/home-page/home-page.component';
+import { TestPageComponent } from './modules/test/test-page/test-page.component';
 
 export const routes: Routes = [
   { path: '', component: HomePageComponent },
+  { path: 'test', component: TestPageComponent },
 ];
