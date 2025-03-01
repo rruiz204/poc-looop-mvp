eb9e3e529f0a36804d5696627c9829270962d149
roney ruiz <roneyruizrojas@gmail.com>
2025-03-01 10:56:53 -0600

    chore(client): remove all files

--- client/src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { HomePageComponent } from './modules/home/home-page/home-page.component';
-import { TestPageComponent } from './modules/test/test-page/test-page.component';
-
-export const routes: Routes = [
-  { path: '', component: HomePageComponent },
-  { path: 'test', component: TestPageComponent },
-];
