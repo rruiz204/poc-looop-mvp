876791bbd2de5ac289027d64126a9b4e850bd441
roney ruiz <roneyruizrojas@gmail.com>
2025-03-02 12:38:31 -0600

    feat(client): build home page component with css

--- client/src/app/modules/home/home-page/home-page.component.ts
+++ client/src/app/modules/home/home-page/home-page.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
   selector: 'app-home-page',
   imports: [],
   templateUrl: './home-page.component.html',
-  styleUrl: './home-page.component.scss'
+  styleUrl: './home-page.component.css'
 })
 export class HomePageComponent {
 
