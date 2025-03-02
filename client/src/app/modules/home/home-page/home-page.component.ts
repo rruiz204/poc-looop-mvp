8f469597785d57042019eeb92e77757b6366552b
roney ruiz <roneyruizrojas@gmail.com>
2025-03-02 12:44:44 -0600

    feat(client): setup prime ng

--- client/src/app/modules/home/home-page/home-page.component.ts
+++ client/src/app/modules/home/home-page/home-page.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { DatePicker } from 'primeng/datepicker';
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [DatePicker],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
