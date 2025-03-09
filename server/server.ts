64f075bdf456062537b1d2c34617ba2b138a0590
roney ruiz <roneyruizrojas@gmail.com>
2025-03-08 21:53:11 -0600

    feat(server): setup server file

--- server/server.ts
+++ server/server.ts
@@ -1,1 +1,15 @@
-console.log("Hello via Bun!");
+import express from "express";
+import { Bootstrap } from "bootstrap";
+import { ExpressConfig } from "@Configs/ExpressConfig";
+
+const { EXPRESS_PORT } = ExpressConfig;
+
+const app = express();
+
+const bootstrap = new Bootstrap(app);
+
+bootstrap.addRouters();
+
+app.listen(EXPRESS_PORT, () => {
+  console.log(`Server is listening at http://localhost:${EXPRESS_PORT}`);
+});
