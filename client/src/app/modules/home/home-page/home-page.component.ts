0ecfa3a547977aaea1191125a05da71e24b3d39c
roney ruiz <roneyruizrojas@gmail.com>
2025-03-08 21:30:48 -0600

    feat(server): two way binding and side effects

--- client/src/app/modules/home/home-page/home-page.component.ts
+++ client/src/app/modules/home/home-page/home-page.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
-import { DatePicker } from 'primeng/datepicker';
+import { Component, signal, effect } from '@angular/core';
+import { FormsModule } from "@angular/forms";
 
 @Component({
   selector: 'app-home-page',
-  imports: [DatePicker],
+  imports: [FormsModule],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
 export class HomePageComponent {
+  public message = signal<string>("");
 
-}
+  private loggingMessageEffect = effect(() => {
+    console.log(`Message: ${this.message()}`);
+  });
+};
