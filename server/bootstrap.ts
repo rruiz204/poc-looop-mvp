8eba5bdfdeb0398941ce072323ffb83f10ba0477
roney ruiz <roneyruizrojas@gmail.com>
2025-03-08 21:52:58 -0600

    feat(server): build ping router and bootstrap

--- server/bootstrap.ts
+++ server/bootstrap.ts
@@ -1,1 +1,11 @@
-console.log("this is my bootstrap file");
+import type { Express } from "express";
+
+import { PingRouter } from "@Routers/PingRouter";
+
+export class Bootstrap {
+  constructor(private app: Express) {};
+
+  public addRouters(): void {
+    this.app.use("/api/ping", PingRouter);
+  };
+};
