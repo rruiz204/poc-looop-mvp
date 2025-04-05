7c52efcdb00c111de01760699e6b0a155c7d14de
roney ruiz <roneyruizrojas@gmail.com>
2025-04-04 21:42:20 -0600

    chore(server): remove old server project

--- server/src/Infrastructure/Services/Logging/LoggingService.ts
+++
@@ -1,9 +0,0 @@
-import winston from "winston";
-
-export class LoggingService {  
-  public static getLogger(): winston.Logger {
-    return winston.createLogger({
-      level: "info",
-    });
-  };
-};
