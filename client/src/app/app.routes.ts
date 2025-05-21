c771eac18ef93ed833003260b6c5d9f8a7c3fc6d
roney ruiz <roneyruizrojas@gmail.com>
2025-05-21 14:14:46 -0600

    chore: remove old client project

--- client/src/app/app.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Routes } from '@angular/router';
-import { HomePageComponent } from './modules/home/home-page/home-page.component';
-
-export const routes: Routes = [
-  { path: "", component: HomePageComponent },
-];
